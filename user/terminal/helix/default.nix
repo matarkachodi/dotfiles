0682255113ea6cd301d517201541b6acd085b9cf
matarkachodi <matarkachodi@gmail.com>
2025-02-21 15:09:22 +0530

    dotfiles: user: helix: Show diagnostics and line numbers on the gutter

--- user/terminal/helix/default.nix
+++ user/terminal/helix/default.nix
@@ -16,6 +16,7 @@
         color-modes = true;
         text-width = 100;
         end-of-line-diagnostics = "hint";
+        gutters = ["diagnostics" "spacer" "line-numbers" "spacer"];
         lsp = {
           display-inlay-hints = true;
           display-progress-messages = true;
