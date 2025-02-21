bf55ded195b5a7f5f3b19b1a07f68b3b4b497a87
matarkachodi <matarkachodi@gmail.com>
2025-02-21 13:09:19 +0530

    dotfiles: user: kitty: Set font family to SauceCodePro Nerd Font

--- user/terminal/emulators/kitty.nix
+++ user/terminal/emulators/kitty.nix
@@ -2,5 +2,12 @@
   # Configure Kitty
   programs.kitty = {
     enable = true;
+    settings = {
+      font_size = "10.8";
+      bold_font = ''family="SauceCodePro Nerd Font" style=Bold'';
+      font_family = ''family="SauceCodePro Nerd Font" style=SemiBold'';
+      italic_font = ''family="SauceCodePro Nerd Font" style="SemiBold Italic"'';
+      bold_italic_font = ''family="SauceCodePro Nerd Font" style="Bold Italic"'';
+    };
   };
 }
