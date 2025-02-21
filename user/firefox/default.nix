0e5ec3831b8e141c91a8c3bef3cb3d588085e4e3
matarkachodi <matarkachodi@gmail.com>
2025-02-21 15:24:05 +0530

    dotfiles: user: firefox: Disable irritating first-run stuff

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -5,6 +5,22 @@
     profiles = {
       me = {
         isDefault = true;
+        settings = {
+          # Disable irritating first-run stuff
+          "browser.disableResetPrompt" = true;
+          "browser.download.panel.shown" = true;
+          "browser.feeds.showFirstRunUI" = false;
+          "browser.messaging-system.whatsNewPanel.enabled" = false;
+          "browser.rights.3.shown" = true;
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.shell.defaultBrowserCheckCount" = 1;
+          "browser.startup.homepage_override.mstone" = "ignore";
+          "browser.uitour.enabled" = false;
+          "startup.homepage_override_url" = "";
+          "trailhead.firstrun.didSeeAboutWelcome" = true;
+          "browser.bookmarks.restore_default_bookmarks" = false;
+          "browser.bookmarks.addedImportButton" = true;
+        };
       };
     };
   };
